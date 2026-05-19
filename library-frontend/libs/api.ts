189dac0ca16e5c6c920acb29c50cb443199ab100
Nookie31247 <dudanmam@gmail.com>
2026-05-19 16:52:54 +0900

    임시 수정(ali url 하드코딩)

--- library-frontend/libs/api.ts
+++ library-frontend/libs/api.ts
@@ -1,6 +1,8 @@
 
-const BASE_URL = typeof window === 'undefined'
-  ? process.env.API_URL || 'http://localhost:8080'  // 서버: 백엔드 직접
-  : '';                                         // 클라이언트: rewrites 통해서
+// const BASE_URL = typeof window === 'undefined'
+//   ? process.env.API_URL || 'http://localhost:8080'  // 서버: 백엔드 직접
+//   : '';                                         // 클라이언트: rewrites 통해서
 
+
+const BASE_URL = "http://library-elb-env-backend.eba-hzy8hta8.ap-northeast-2.elasticbeanstalk.com"
 export const apiUrl = (path: string) => `${BASE_URL}/api${path}`;
